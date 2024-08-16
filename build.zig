f055fbae437c8ecf5834a7c8681ff66b67998723
Kristian Larsson <kristian@spritelink.net>
2024-08-16 14:10:45 +0200

    Some build.zig improvements
    Instruct Zigs ConfigHeader generator to use the existing config.h.in in
    CMake style to avoid repetition of a lot of the variables.
    
    Set a minimum set of variables to get this thing to build.
    
    Basic support for building as static / shared lib, copying
    BUILD_SHARED_LIBS option from CMake config.
    
    Pull in libyang as a zig package dep (I wrote a build.zig for it
    separately). Also pcre2, which has a build.zig from upstream repo but
    was missing build.zig.zon, so using a fork of the repo for now where I
    added that.
    
    Building the sysrepoctl executable to verify that linking and everything
    works correctly.
    
    Fix some indent.

--- build.zig
+++ build.zig
@@ -5,39 +5,62 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const build_shared_libs = b.option(bool, "BUILD_SHARED_LIBS",
+                "Build shared libraries (otherwise static ones)") orelse true;
+
+    const dep_libyang = b.dependency("libyang", .{
+        .target = target,
+        .optimize = optimize,
+        .BUILD_SHARED_LIBS = build_shared_libs,
+    });
+
+    const dep_pcre2 = b.dependency("pcre2", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = .static,
+    });
+
+
+    var lib = b.addStaticLibrary(.{
         .name = "sysrepo",
         .target = target,
         .optimize = optimize,
     });
+    if (build_shared_libs) {
+        lib = b.addSharedLibrary(.{
+            .name = "sysrepo",
+            .target = target,
+            .optimize = optimize,
+        });
+    }
 
     const source_files = [_][]const u8{
-	"src/sysrepo.c",
-    	"src/common.c",
-	"src/ly_wrap.c",
-	"src/subscr.c",
-	"src/log.c",
-	"src/replay.c",
-	"src/modinfo.c",
-	"src/edit_diff.c",
-	"src/lyd_mods.c",
-	"src/context_change.c",
-	"src/shm_main.c",
-	"src/shm_ext.c",
-	"src/shm_mod.c",
-	"src/shm_sub.c",
-	"src/sr_cond/sr_cond_futex.c",
-	"src/plugins/ds_json.c",
-	"src/plugins/ntf_json.c",
-	"src/plugins/common_json.c",
-	"src/utils/values.c",
-	"src/utils/xpath.c",
-	"src/utils/error_format.c",
-	"src/utils/nacm.c",
-	"src/utils/subscribed_notifications.c",
-	"src/utils/sn_common.c",
-	"src/utils/sn_yang_push.c",
-	};
+        "src/sysrepo.c",
+        "src/common.c",
+        "src/ly_wrap.c",
+        "src/subscr.c",
+        "src/log.c",
+        "src/replay.c",
+        "src/modinfo.c",
+        "src/edit_diff.c",
+        "src/lyd_mods.c",
+        "src/context_change.c",
+        "src/shm_main.c",
+        "src/shm_ext.c",
+        "src/shm_mod.c",
+        "src/shm_sub.c",
+        "src/sr_cond/sr_cond_futex.c",
+        "src/plugins/ds_json.c",
+        "src/plugins/ntf_json.c",
+        "src/plugins/common_json.c",
+        "src/utils/values.c",
+        "src/utils/xpath.c",
+        "src/utils/error_format.c",
+        "src/utils/nacm.c",
+        "src/utils/subscribed_notifications.c",
+        "src/utils/sn_common.c",
+        "src/utils/sn_yang_push.c",
+    };
 
     lib.addCSourceFiles(.{
         .files = &source_files,
@@ -46,55 +69,97 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    const config_header = b.addConfigHeader(
-        .{
-            .style = .blank,
+    const config_header = b.addConfigHeader(.{
+        .style = .{ .cmake = b.path("src/config.h.in") },
+        .include_path = "config.h"
+        }, .{
+        .INTERNAL_MODULE_DATA_FORMAT = "LYD_XML", // TODO: option? JSON?
+        .NACM_RECOVERY_USER="root", // TODO: option
+        .REPO_PATH="./repository", // TODO: improve?
+        .SHM_DIR = "/dev/shm", // TODO: auto-detect freebsd = "/tmp/shm"
+        .SYSREPO_SUPERUSER_UID=0, // TODO: option
+        .SYSREPO_UMASK="000", // TODO: option?
+
+        .DEFAULT_CANDIDATE_DS_PLG = "JSON DS file",
+        .DEFAULT_FACTORY_DEFAULT_DS_PLG = "JSON DS file",
+        .DEFAULT_NOTIFICAITON_DEFAULT_DS_PLG = "JSON DS file",
+        .DEFAULT_OPERATIONAL_DS_PLG = "JSON DS file",
+        .DEFAULT_RUNNING_DS_PLG = "JSON DS file",
+        .DEFAULT_STARTUP_DS_PLG = "JSON DS file",
+        .NACM_SRMON_DATA_PERM = "600",
+        .SR_COND_IMPL = "sr_cond_futex", // TODO: auto-detect maybe?
+        .SR_HAVE_DLOPEN = true, // TODO: auto-detect
+        .SR_HAVE_EACCESS = true, // TODO: auto-detect
+        .SR_PLUGINS_PATH = "/usr/local/lib/sysrepo/plugins",
+        .YANGLIB_REVISION = "2019-01-04",
         },
-        .{
-	    .SR_HAVE_EACCESS = true,
-            .SR_HAVE_DLOPEN = true,
-            .SR_PLG_PATH = "/usr/local/lib/sysrepo/plugins",
-            .SR_PLG_SUFFIX = ".so",
-            .SR_PLG_SUFFIX_LEN = 3,
-            .SR_SU_SUID = 0,
-            .SR_NACM_RECOVERY_USER = "root",
-            //.SR_YANGLIB_REVISION = 2019-01-04,
-            .SR_REPO_PATH = "./build/repository",
-            .SR_REPO_PATH_ENv = "SYSREPO_REPOSITORY_PATH",
-            .SR_STARTUP_PATH = "",
-            .SR_FACTORY_DEFAULT_PATH = "",
-            .SR_NOTIFICATION_PATH = "",
-            .SR_YANG_PATH = "",
-            .SR_INIT_MOD_DATA = "",
-            //.SR_INIT_MOD_DATA_FORMAT = LYD_XML ,
-            .SR_INT_MOD_DISABLED_RUNNING = "",
-            .SR_SHM_DIR = "/dev/shm",
-            .SR_SHM_PREFIX_DEFAULT = "sr",
-            .SR_SHM_PREFIX_ENV = "SYSREPO_SHM_PREFIX",
-            .SR_GROUP = "",
-            //.SR_UMASK = 00000,
-            //.SR_DIR_PERM = 00777,
-            //.SR_PLG_DIR_PERM = 00770,
-            //.SR_YANG_PERM = 00644,
-            //.SR_FILE_PERM = 00600,
-            //.SR_INTMOD_MAIN_FILE_PERM = 00666,
-            //.SR_INTMOD_NACM_SRMON_FILE_PERM = 00600,
-            //.SR_INTMOD_NODATA_FILE_PERM = 00444,
-            //.SR_INTMOD_WITHDATA_FILE_PERM = 00644,
-            .SR_DEFAULT_STARTUP_DS = "JSON DS file",
-            .SR_DEFAULT_RUNNING_DS = "JSON DS file",
-            .SR_DEFAULT_CANDIDATE_DS = "JSON DS file",
-            .SR_DEFAULT_OPERATIONAL_DS = "JSON DS file",
-            .SR_DEFAULT_FACTORY_DEFAULT_DS = "JSON DS file",
-            .SR_DEFAULT_NOTIFICATION_DS = "JSON DS file",
-        },
-    );
+                                            );
+
+    const compat_header = b.addConfigHeader(.{
+        .style = .{ .cmake = b.path("compat/compat.h.in") },
+        .include_path = "compat.h"
+        }, .{
+        .HAVE_STRDUPA=1,
+        .HAVE_CLOCK_MONOTONIC=1, // TODO: auto-detect, remove this because we directly set below?
+        .COMPAT_CLOCK_ID="CLOCK_MONOTONIC", // TODO: auto-detect
+    });
+
+    const version_header = b.addConfigHeader(.{
+        .style = .{ .cmake = b.path("src/version.h.in") },
+        .include_path = "version.h"
+        }, .{
+        .SYSREPO_MAJOR_SOVERSION=7,
+        .SYSREPO_MINOR_SOVERSION=27,
+        .SYSREPO_MICRO_SOVERSION=1,
+        .SYSREPO_SOVERSION_FULL="7.27.1",
+        .SYSREPO_VERSION="7",
+    });
+
+    const bin_common_header = b.addConfigHeader(.{
+        .style = .{ .cmake = b.path("src/executables/bin_common.h.in") },
+        .include_path = "bin_common.h"
+        }, .{
+    });
+
 
     lib.addConfigHeader(config_header);
+    lib.addConfigHeader(compat_header);
+    lib.addConfigHeader(version_header);
     lib.addIncludePath(b.path("src"));
     lib.addIncludePath(b.path("src/plugins"));
-    lib.linkSystemLibrary("libyang");
+    lib.linkLibrary(dep_libyang.artifact("yang"));
+    lib.linkLibrary(dep_pcre2.artifact("pcre2-8"));
+    //lib.linkSystemLibrary("libyang");
     lib.linkLibC();
-    //b.installFile(".h", "include/utf8proc.h");
+
+    lib.installLibraryHeaders(dep_libyang.artifact("yang"));
+    lib.linkLibrary(dep_libyang.artifact("yang"));
+
+    lib.installHeader(b.path("src/sysrepo.h"), "sysrepo.h");
+    lib.installHeader(compat_header.getOutput(), "sysrepo/compat.h");
     b.installArtifact(lib);
+
+    const sysrepoctl = b.addExecutable(.{
+        .name = "sysrepoctl",
+        .target = target,
+        .optimize = optimize,
+    });
+    sysrepoctl.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "src/executables/sysrepoctl.c",
+        },
+        .flags = &[_][]const u8{
+        },
+    });
+    sysrepoctl.addConfigHeader(compat_header);
+    sysrepoctl.addConfigHeader(bin_common_header);
+    sysrepoctl.addIncludePath(b.path("src"));
+    sysrepoctl.installLibraryHeaders(lib);
+    sysrepoctl.installLibraryHeaders(dep_libyang.artifact("yang"));
+    sysrepoctl.installLibraryHeaders(dep_pcre2.artifact("pcre2-8"));
+    sysrepoctl.linkLibrary(lib);
+    sysrepoctl.linkLibrary(dep_libyang.artifact("yang"));
+    sysrepoctl.linkLibrary(dep_pcre2.artifact("pcre2-8"));
+    sysrepoctl.linkLibC();
+    b.installArtifact(sysrepoctl);
 }
